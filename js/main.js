e08107cc25e340d7c2b74d51890aeb7e96f4f78c
Melani-Prabodhika <melaniprabodhika8@gmail.com>
2023-05-30 09:02:01 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,79 @@
+// let eye = document.getElementById("eye");
+// let password = document.getElementById("password");
+// let cpassword = document.getElementById("cpassword");
+// let showPassword = false;
+
+// eye.addEventListener("click",function(){
+//   // password.setAttribute("type","text");
+//   if(showPassword == false){
+//    if (password.type === "password") {
+//      password.type = "text";
+//    } else {
+//      password.type = "password";
+//    }
+//    eye.classList.add("fa-eye-slash");
+//    eye.classList.remove("fa-eye");
+//    showPassword = true;
+//   }else{
+//     if (password.type === "text") {
+//       password.type = "password";
+//     } else {
+//       password.type = "text";
+//     }
+//     eye.classList.remove("fa-eye-slash");
+//     eye.classList.add("fa-eye");
+//     showPassword = false;
+//   }
+// });
+
+let eye = document.getElementById("eye");
+let ceye = document.getElementById("ceye");
+let password = document.getElementById("password");
+let cpassword = document.getElementById("cpassword");
+let showPassword = false;
+let hidePassword = false;
+
+eye.addEventListener("click",function(){
+  // password.setAttribute("type","text");
+  if(showPassword == false){
+   if (password.type === "password") {
+     password.type = "text";
+   } else {
+     password.type = "password";
+   }
+   eye.classList.add("fa-eye-slash");
+   eye.classList.remove("fa-eye");
+   showPassword = true;
+  }else{
+    if (password.type === "text") {
+      password.type = "password";
+    } else {
+      password.type = "text";
+    }
+    eye.classList.remove("fa-eye-slash");
+    eye.classList.add("fa-eye");
+    showPassword = false;
+  }
+});
+
+ceye.addEventListener("click",function(){
+  if(hidePassword == false){
+   if (cpassword.type === "password") {
+     cpassword.type = "text";
+   } else {
+     cpassword.type = "password";
+   }
+   ceye.classList.add("fa-eye-slash");
+   ceye.classList.remove("fa-eye");
+   hidePassword = true;
+  }else{
+    if (cpassword.type === "text") {
+      cpassword.type = "password";
+    } else {
+      cpassword.type = "text";
+    }
+    ceye.classList.remove("fa-eye-slash");
+    ceye.classList.add("fa-eye");
+    hidePassword = false;
+  }
+});
